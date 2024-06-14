e0c9f081415223dce6cbceb2f90d902c5464bb3d
i.kłusek <J6ZkqwVUn>
2024-06-14 21:06:59 +0200

    NJS-23-Uploading image with multer, and saving it to folder

---
+++ 3.Basics/config/multer.config.ts
@@ -0,0 +1,55 @@
+import multer, { FileFilterCallback } from 'multer';
+import { Request } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+const postFolderPath = path.join(process.cwd(), 'images');
+
+// Ensure the 'images' directory exists
+const ensureDirectoryExistence = (dir: string) => {
+  if (!fs.existsSync(dir)) {
+    console.log('Creating new folder');
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
+
+// Ensure the directory exists before configuring Multer
+ensureDirectoryExistence(postFolderPath);
+
+const storage = multer.diskStorage({
+  destination: (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) => {
+    cb(null, postFolderPath);
+  },
+  filename: (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void
+  ) => {
+    const fileExtension: string = path.extname(file.originalname);
+    const fileName: string = uuidv4() + fileExtension;
+    cb(null, fileName);
+  },
+});
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const settings: multer.Options = { storage, fileFilter };
+
+export default {
+  settings,
+};
--- 3.Basics/readme.md
+++ 3.Basics/readme.md
@@ -1,2 +1,3 @@
 package used:
 csrf: https://www.npmjs.com/package/csurf
+multer: https://www.npmjs.com/package/multer
