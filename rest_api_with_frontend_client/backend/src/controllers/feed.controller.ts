e2941aa7c200135b2c2cc30889c32c25066280b1
i.kłusek <J6ZkqwVUn>
2024-06-22 09:42:17 +0200

    NJS-33- Connect backend and frontend + basic validation

--- rest_api_with_frontend_client/backend/src/controllers/feed.controller.ts
+++ rest_api_with_frontend_client/backend/src/controllers/feed.controller.ts
@@ -1,17 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
 function getPosts(req: Request, res: Response, next: NextFunction) {
-  res.status(200).json({ posts: [{ title: 'First post', content: 'Elo' }] });
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Igor',
+        },
+        date: new Date(),
+      },
+    ],
+  });
 }
 
 function createPost(req: Request, res: Response, next: NextFunction) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
+  }
+
   const { title, content } = req.body;
 
   //Create post in db;
 
   res.status(201).json({
     message: 'Post created successfully',
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: 'Igor' },
+      createdAt: new Date(),
+    },
   });
 }
 
--- rest_api_with_frontend_client/backend/src/routes/feed.router.ts
+++ rest_api_with_frontend_client/backend/src/routes/feed.router.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { body } from 'express-validator';
+
 import feedController from '../controllers/feed.controller';
 
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
-router.post('/post', feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 export default router;
